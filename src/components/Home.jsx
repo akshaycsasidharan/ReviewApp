b013ad32019ae418aadc9a1dce93792c41f68d22
AKSHAY .C.S <akshaycsasidharan@gmail.com>
2024-09-26 22:08:18 -0700

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Review Blog App</h1>
+
+    <div className="container">
+
+        <img src="" alt="" />
+
+        <h3>suasan smith</h3>
+
+        <h3>WEb developer</h3>
+
+        <p>kaljdkjkfjdskj fdksjfnkdjvd fkjdfhsdkf fkjdhkfjsh fjhsdfkgshf kjfshgjsfh  </p>
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Home
